8c6f00e88ea1d6660cfab3335c0cdc86daffa310
masi23 <mateuszzwierzchowski19@gmail.com>
2025-12-22 19:18:39 +0100

    Simple server security implemented.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
-// import { PrismaClient } from "./generated/prisma/client";
-import { PrismaClient } from "@prisma/client";
+const cors = require("cors");
+import helmet from "helmet";
 import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/error-handler";
 import UserRouter from "./routers/user.route";
@@ -15,9 +15,10 @@ import CommentRouter from "./routers/comment.route";
 dotenv.config();
 
 const app = express();
-// const prisma = new PrismaClient();
 
-app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
   res.json("Hello World");
